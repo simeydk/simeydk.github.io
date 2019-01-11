6f49b45f6d2cbabd07a4f0500c2eb4590c7b8280
Simey de Klerk <simeydeklerk@gmail.com>
2019-01-11 12:39:03 +0200

    fix today's date

--- baby/js/index.js
+++ baby/js/index.js
@@ -33,6 +33,7 @@ function dateDiffWeeksAndDays(a,b = new Date()) {
 function renderHTML() {
   return `
   <main id="page">
+    <p class="days_left">${now.toUTCString().slice(0,16)}</p>
     <div class="weeks_and_days">
       <p class="element">
         <span class="atom num week_num">${toNow.weeks}</span>
@@ -55,63 +56,3 @@ function toggleClassForId(id,className) {
   document.getElementById(id).classList.toggle(className)
 }
 
-
-
-
-function monthsSince2010(date) {
-    return  (date.getFullYear() - 2010) * 12 + date.getMonth()
-}
-
-function dateDiffFullMonths(start,end) {
-    if (start > end) {return dateDiffFullMonths(end,start)}
-
-    const mStart = monthsSince2010(start)
-    const mEnd = monthsSince2010(end)
-    const mDiff = mEnd - mStart
-    
-    const dStart = start.getDate()
-    const dEnd = end.getDate()
-
-    mDiffFinal = mDiff - ((dEnd - dStart < 0) ? 1 : 0)
-    
-    return mDiffFinal
-
-}
-
-function dateDiffMonthsAndDays(start,end) {
-    
-    if (start > end) {return dateDiffMonthsAndDays(end,start)}
-
-    const months = dateDiffFullMonths(start,end)
-    const startPlusMonths = startOfDay(addMonths(start,months))
-    const days = Math.floor((end - startPlusMonths) / oneDay)
-
-    return {months,days, startPlusMonths}
-
-}
-
-function addMonths(date,m) {
-    return new Date(
-        date.getFullYear(), 
-        date.getMonth() + m,
-        date.getDate()
-        )
-}
-
-function dateAdd(date,years=0,months=0,days=0,hours=0,minutes=0,seconds=0,milliseconds=0) {
-    
-    return new Date(
-        date.getFullYear() + years, 
-        date.getMonth() + months,
-        date.getDate() + days,
-        d.getHours() + hours,
-        d.getMinutes() + minutes,
-        d.getSeconds() + seconds,
-        d.getMilliseconds() + milliseconds
-    )
-
-}
-
-function startOfDay(date) {
-    return new Date(date.getFullYear(), date.getMonth(), date.getDate())
-}
